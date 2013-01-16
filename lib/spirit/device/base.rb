f236fb09c7298fca331564d4ac6c5e7a3e854300
Nick Rowe <nixterrimus@dcxn.com>
2013-01-15 22:27:27 -0800

    A device may be initialized with a configuration object

--- lib/spirit/device/base.rb
+++ lib/spirit/device/base.rb
@@ -1,15 +1,20 @@
 module Device
   class Base
-    attr_accessor :adapter
+    attr_accessor :adapter, :configuration
 
     def initialize(params = {})
       @adapter = params.fetch(:adapter, default_adapter)
+      @configuration = params.fetch(:configuration, default_configuration)
     end
 
     private
 
     def default_adapter
       Adapter::NilAdapter.new
+    end
+
+    def default_configuration
+      {}
     end
   end
 end
