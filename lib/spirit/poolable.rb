45cca1fcf87503b48e4bb40e2042135c3305cc66
Nick Rowe <nixterrimus@dcxn.com>
2013-01-27 17:39:26 -0800

    Poolable can includes default implementation of after_add and after_remove

--- lib/spirit/poolable.rb
+++ lib/spirit/poolable.rb
@@ -7,14 +7,29 @@ module Poolable
 
   def add(device)
     self.pool[device.uuid] = device
+    self.after_add(device)
+  end
+
+  def after_add(device)
+    nil
   end
 
   def remove(device)
     self.pool.delete(device.uuid)
+    self.after_remove(device)
+  end
+
+  def after_remove(device)
+    nil
   end
 
   def each(&block)
     self.pool.values.each(&block)  
   end
+
+  def pool_uuids
+   self.pool.keys
+  end
+
   
 end
