9046a7e8d96c8b935cd6adce7a5c7536a7b6e647
Nick Rowe <nixterrimus@dcxn.com>
2013-01-29 21:24:31 -0800

    First pass at spirit spec

---
+++ spec/spirit_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe Spirit do
+  describe 'devices' do
+    let(:devices) { 3.times.collect { Device::Light.new } }
+    context 'when there are no devices' do
+      it 'is empty' do
+        expect(Spirit.devices).to be_empty
+      end
+      context 'when there are devices' do
+        it 'returns a list of devices' do
+          devices.each { |d| Spirit.devices <<  d }
+          expect(Spirit.devices.length).to be(3)
+          devices.each do |device|
+            expect(Spirit.devices.include? device).to be_true
+          end
+        end
+      end
+    end
+  end
+
+  describe 'persistance' do
+    it 'returns an object store' do
+      expect(Spirit.persistance.class).to be(ObjectStore)
+    end
+
+    it 'always returns the same instance' do
+      expect(Spirit.persistance).to be(Spirit.persistance)
+    end
+  end
+
+end
