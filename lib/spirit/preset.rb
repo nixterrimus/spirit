8fdf3d7b0ccddbfdeae5ac0d4da6d3ada7757eca
Nick Rowe <nixterrimus@dcxn.com>
2013-02-05 22:33:46 -0800

    Devices can be added to presets

--- lib/spirit/preset.rb
+++ lib/spirit/preset.rb
@@ -1,3 +1,17 @@
 class Preset
   include ::Identifiable
+
+  def add_device(device)
+    device_target_attributes[device.uuid] = {}
+  end
+
+  def device_uuids
+    device_target_attributes.keys
+  end
+
+  private
+
+  def device_target_attributes
+    @devices ||= Hash.new
+  end
 end
--- spec/preset_spec.rb
+++ spec/preset_spec.rb
@@ -2,4 +2,21 @@ require 'spec_helper'
 
 describe Preset do
   it_should_behave_like Identifiable, Preset.new
+  subject { Preset.new }
+  let(:device) { Device::Light.new }
+
+  describe 'adding a device' do
+    it 'stores the devices empheral attributes' do
+      pending
+    end
+  end
+
+  describe 'device_uuids' do
+    before do
+      subject.add_device(device)
+    end
+    it 'returns a list of all devices that are part of the preset' do
+      subject.device_uuids.should include device.uuid
+    end
+  end
 end
