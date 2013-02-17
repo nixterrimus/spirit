89260d09038aea36f3030935360fb8b9012a49b2
Nick Rowe <nixterrimus@dcxn.com>
2013-02-16 18:00:55 -0800

    Light Spec

--- spec/spirit/device/light_spec.rb
+++ spec/spirit/device/light_spec.rb
@@ -1,8 +1,7 @@
 require 'spec_helper'
-require 'spirit/device/ability/switchable_spec'
 
 describe Light do
-  subject { Device::Light.new }
+  subject { Light.new }
 
-  it_should_behave_like Abilities::Switchable, Light.new
+  its(:abilities) { should(include 'switchable') }
 end
