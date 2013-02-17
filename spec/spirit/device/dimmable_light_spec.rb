f5d63e3cc03d8af414d7dde87167bfae9d1151ec
Nick Rowe <nixterrimus@dcxn.com>
2013-02-16 18:00:47 -0800

    DimmableLight spec

--- spec/spirit/device/dimmable_light_spec.rb
+++ spec/spirit/device/dimmable_light_spec.rb
@@ -1,12 +1,10 @@
 require 'spec_helper'
-require 'spirit/device/ability/switchable_spec'
-require 'spirit/device/ability/dimmable_spec'
 
 describe DimmableLight do
-  subject { Device::DimmableLight.new }
+  subject { DimmableLight.new }
 
-  it_should_behave_like Abilities::Switchable, DimmableLight.new
-  it_should_behave_like Abilities::Dimmable, DimmableLight.new  
+  its(:abilities) { should(include 'switchable') }
+  its(:abilities) { should(include 'dimmable') }
   
 end
 
