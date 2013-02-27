36c4f0937e8d8a800b271fc9ba1c2b021b60aa84
Nick Rowe <nixterrimus@dcxn.com>
2013-02-27 00:00:28 -0800

    Adapter worker should read id using Device class

--- lib/spirit/workers/adapter.rb
+++ lib/spirit/workers/adapter.rb
@@ -3,7 +3,7 @@ module Worker
     include SuckerPunch::Worker
 
     def perform(device_id, adapter_id)
-      device = ColorableLight.read(device_id)
+      device = Device.read(device_id)
       adapter = Adapter::HueAdapter.read(adapter_id)
       adapter.apply(device.attributes)
     end
