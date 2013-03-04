5d75f2a40cec5c19a9876d30e15e3a702219c721
Nick Rowe <nixterrimus@dcxn.com>
2013-03-04 15:15:37 -0800

    Action and spec
    Action is an abstract class, it feels unruby-like to commit this so it may get factored out later.  The idea is to have objects that respond to apply and take an event that actually do things.

--- lib/spirit/triggers/action.rb
+++ lib/spirit/triggers/action.rb
@@ -2,6 +2,8 @@ class Action
   include Toy::Store
 
   def apply
-
+    # It is overridden by children and applies the action
+    # apply the action
   end
 end
+
---
+++ spec/spirit/triggers/action_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Action do
+  describe "#apply" do
+    # It is overridden by children
+  end
+end
