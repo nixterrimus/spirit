2e0f1368f26328974572b2baed9cb234e76b531d
Nick Rowe <nixterrimus@dcxn.com>
2013-03-16 01:47:46 -0700

    Refactor api server to use a real spirit object

--- lib/spirit_server/server.rb
+++ lib/spirit_server/server.rb
@@ -1,22 +1,26 @@
 class SpiritHTTPServer < Sinatra::Base
   get '/devices' do
     content_type :json
-    devices = Spirit.devices
+    devices = spirit.devices
     serializer = ActiveModel::ArraySerializer.new(devices, each_serializer: DeviceSerializer)
     serializer.to_json
   end
 
   get '/devices/:id' do
     content_type :json    
-    device = Device.find(params[:id])
+    device = environment.find(params[:id])
     serializer = DeviceSerializer.new(device)
     serializer.to_json
   end
 
   get '/presets' do
     content_type :json
-    presets = Spirit.presets
+    presets = spirit.presets
     serializer = ActiveModel::ArraySerializer.new(presets, each_serializer: PresetSerializer)
     serializer.to_json
+  end
+
+  def spirit
+    settings.spirit
   end
 end
