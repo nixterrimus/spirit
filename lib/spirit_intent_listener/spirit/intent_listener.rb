f47fdbf7f43bd7744f9051f27846acc0044015a4
Nick Rowe <nixterrimus@dcxn.com>
2013-03-17 02:13:05 -0700

    A working intent listener

--- lib/spirit_intent_listener/spirit/intent_listener.rb
+++ lib/spirit_intent_listener/spirit/intent_listener.rb
@@ -6,7 +6,44 @@ module Spirit
     end
 
     def listen
-      sleep
+      redis.psubscribe(channel_to_listen_on) do |c|
+        c.pmessage { |ptrn, channel, message| intent_received(channel, message) }
+      end
     end
+
+    def intent_received(intent_identifier, params)
+      logger.info "Intent: #{intent_identifier} | params: #{params}"
+      intent_for(intent_identifier).apply(parse_params(params))
+    rescue
+      logger.error "Failed to apply #{intent_identifier}"
+    end
+
+
+    private
+
+    def channel_to_listen_on
+      'intents.spirit.*'
+    end
+
+    def redis
+      @redis ||= Redis.new
+    end
+
+    # This might be better as a factory
+    def intent_for(channel)
+      root_intent_class = channel.split(".").last.classify
+      klass = Object.const_get("Intent").const_get(root_intent_class)
+      klass.new
+    end
+
+    # Too much happening in this class, this belong elsewhere
+    def parse_params(params)
+      JSON.parse(params)
+    end
+
+    def logger
+      @logger ||= Logger.new(STDOUT)
+    end
+
   end
 end
