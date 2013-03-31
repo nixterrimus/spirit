4ab845a8c47a725ad827819307ad4362beec6a21
Nick Rowe <nixterrimus@dcxn.com>
2013-03-31 16:01:14 -0700

    In a responder when params are parsed, they are stored

--- lib/spirit_intent_listener/responder.rb
+++ lib/spirit_intent_listener/responder.rb
@@ -1,6 +1,11 @@
 class Responder
-  attr_reader :params
+  attr_reader :params, :parsed_params
   def initialize(params)
     @params = params
   end
+
+  def parse_params
+    @parsed_params = JSON.parse(params).with_indifferent_access
+  end
+
 end
--- spec/spirit_intent_listener/responder_spec.rb
+++ spec/spirit_intent_listener/responder_spec.rb
@@ -2,10 +2,22 @@ require 'spec_helper'
 
 describe Responder do
   let(:params) { '{"preset": {"id": "718bbaa6-85f1-11e2-935e-d611d120ea23"}}' }
+  let(:subject) { Responder.new(params) }
   describe "initialization" do
     it 'receives a string of the params and stores it' do
       subject = Responder.new(params)
       expect(subject.params).to eql(params)
+    end
+  end
+
+  describe '#parse_params' do
+    it 'stores the pared params to parsed_params' do
+      expect(subject.parsed_params).to eql(nil)
+      subject.parse_params
+      expect(subject.parsed_params).to_not eql(nil)
+    end
+    it 'decodes the params' do
+      pending
     end
   end
 end
